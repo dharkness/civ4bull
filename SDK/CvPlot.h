132d389aa0727875976fb83506482493045f1620
David Harkness <341319+dharkness@users.noreply.github.com>
2009-11-09 04:18:24 +0000

    Lat/Long Coordinates use full range when not wrapping

--- SDK/CvPlot.h
+++ SDK/CvPlot.h
@@ -213,8 +213,9 @@ public:
 #endif
 	bool at(int iX, int iY) const;																																		// Exposed to Python
 // BUG - Lat/Long Coordinates - start
-	int getRealLongitudeMinutes() const;																																		// Exposed to Python
-	int getRealLatitudeMinutes() const;																																		// Exposed to Python
+	int calculateMinutes(int iPlotIndex, int iPlotCount, bool bWrap, int iDegreeMin, int iDegreeMax) const;
+	int getLongitudeMinutes() const;																																		// Exposed to Python
+	int getLatitudeMinutes() const;																																		// Exposed to Python
 // BUG - Lat/Long Coordinates - end
 	int getLatitude() const;																																					// Exposed to Python  
 	int getFOWIndex() const;
