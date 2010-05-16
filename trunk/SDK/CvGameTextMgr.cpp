1d63448c62d89ea37a5caa77709f7054eed375aa
emperorfool <emperorfool@53d650ad-4aa8-471b-a6a5-e7a907e09928>
2010-05-16 21:44:42 +0000

    Spies no longer get domain experience from Barracks. Removed extra kPlayer declaration.

--- trunk/SDK/CvGameTextMgr.cpp
+++ trunk/SDK/CvGameTextMgr.cpp
@@ -8322,7 +8322,6 @@ void CvGameTextMgr::setBasicUnitHelpWithCity(CvWStringBuffer &szBuffer, UnitType
 
 		if ((UnitTypes)kCivilization.getCivilizationUnits(eUnitClass) == eUnit && kUnit.canAcquireExperience())
 		{
-			CvPlayer& kPlayer = GET_PLAYER(pCity->getOwnerINLINE());
 			int iExperience;
 
 			iExperience = pCity->getFreeExperience();
@@ -8350,7 +8349,12 @@ void CvGameTextMgr::setBasicUnitHelpWithCity(CvWStringBuffer &szBuffer, UnitType
 				if (eBuilding != NO_BUILDING)
 				{
 					CvBuildingInfo& kBuilding = GC.getBuildingInfo(eBuilding);
-					iExperience = kBuilding.getDomainFreeExperience(eDomainType) + kBuilding.getUnitCombatFreeExperience(eCombatType);
+					iExperience = kBuilding.getUnitCombatFreeExperience(eCombatType);
+					if (!kUnit.isSpy())
+					{
+						iExperience += kBuilding.getDomainFreeExperience(eDomainType);
+					}
+
 					if (iExperience != 0)
 					{
 						if (pCity->getNumBuilding(eBuilding) > 0)
